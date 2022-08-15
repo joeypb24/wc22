e3bdbd5177cd35f59ce226b2babc65537d78cdf0
Joey <joeybo@bss-ph.com>
2022-08-15 18:19:11 +0800

    added tnc-box in prediction

---
+++ tabs-sub-item-sect-prediction.php
@@ -0,0 +1,21 @@
+<section class="wc22-tab-items__item--1__subs__item__sect wc22-tab-items__item--1__subs__item__sect--odds">
+
+	<div class="inner">
+
+		<div class="tnc-box">
+			<img src="./images/wc22/tnc/tnc-char.png" alt="" width="131" height="101">
+			<div class="tnc-box__texts">
+				<h3>Please select your predictions to win</h3>
+				<p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+			</div>
+			<a role="button" class="to-tnc">Terms & Conditions</a>
+			<div class="tnc-box__logos">
+				<img src="./images/wc22/tnc/football-championship-2022.svg" alt="" width="95" height="75">
+				<img src="./images/wc22/tnc/ball.svg" alt="" width="84" height="84">
+			</div>
+		</div>
+
+	</div>
+	<!-- ~ inner -->
+
+</section>

441f67af49193aefefe4550ae2b58629f2781c7c
Joey <joeybo@bss-ph.com>
2022-08-16 16:02:19 +0800

    test broadage

--- tabs-sub-item-3.php
+++ tabs-sub-item-3.php
@@ -1,7 +1,35 @@
 <div class="wc22-tab-items__item--1__subs__item wc22-tab-items__item--1__subs__item--3">
 	
 	<section class="wc22-tab-items__item--1__subs__item__sect">	
-		3
+		<div class="inner">
+
+			<div class="groups" id="groups"></div>
+
+		</div>
 	</section>
+
+	<script>
+		(function(b, s, p, o, r, t) {
+			b["broadage"] = b["broadage"] || [];
+			if (!b["broadage"].length) {
+				r = document.createElement(s);
+				t = document.getElementsByTagName(s)[0];
+				r.async = true;
+				r.src = p;
+				t.parentNode.insertBefore(r, t);
+			}
+			b["broadage"].push({ "bundleId": o.bundleId, "widgets": o.widgets, "accountId": o.accountId });
+		})(window, "script", "//cdn-saas.broadage.com/widgets/loader/loader.js", {
+			"bundleId": ["soccer-fx"],
+			"accountId": "5740fffe-8264-410c-a776-d584587e5de7",
+			"widgets": {
+				"soccerFixture": [{
+					"element": "groups",
+					"tournamentId": 6
+				}]
+			}
+		});
+	</script>
+
 
 </div>

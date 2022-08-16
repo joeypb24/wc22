89baa5fb6661842dcf202ad08e7494b749449ef2
Joey <joeybo@bss-ph.com>
2022-08-16 10:55:05 +0800

    separated header footer carousel

---
+++ footer.php
@@ -0,0 +1,10 @@
+    <div class="footer-wrapper">
+      <div
+        id="placeholder-footer-content"
+        data-url="/footer-content/en-us?device=Desktop&amp;forceDesktop=False"
+      ></div>
+      <div class="loading-icon"></div>
+    </div>
+
+  </body>
+</html>

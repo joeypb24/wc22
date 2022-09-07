c5d98510428cedc9381131afb65edb4aa90ad6e3
Cristian Paul Castañeda <cristianpaul_c@yahoo.com>
2022-09-07 13:57:44 +0400

    Reconstruct timer

--- js/wc22/wc22-paul.js
+++ js/wc22/wc22-paul.js
@@ -1,39 +1,57 @@
 'use strict';
 
-(function () {
-  const second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
+// (function () {
+//   const second = 1000,
+//     minute = second * 60,
+//     hour = minute * 60,
+//     day = hour * 24;
 
-  let today = new Date(),
-    dd = String(today.getDate()).padStart(2, "0"),
-    mm = String(today.getMonth() + 1).padStart(2, "0"),
-    yyyy = today.getFullYear(),
-    nextYear = yyyy + 1,
-    dayMonth = "11/20/",
-    wc = dayMonth + yyyy;
+//   let today = new Date(),
+//     dd = String(today.getUTCDate()).padStart(2, "0"),
+//     mm = String(today.getUTCMonth() + 1).padStart(2, "0"),
+//     yyyy = today.getUTCFullYear(),
+//     nextYear = yyyy + 1,
+//     dayMonth = "11/20/",
+//     wc = dayMonth + yyyy;
 
-  today = mm + "/" + dd + "/" + yyyy;
-  if (today > wc) {
-    wc = dayMonth + nextYear;
-  }
-  //end
+//   today = mm + "/" + dd + "/" + yyyy;
+//   if (today > wc) {
+//     wc = dayMonth + nextYear;
+//   }
+//   //end
 
-  const countDown = new Date(wc).getTime(),
-    x = setInterval(function () {
-      const now = new Date().getTime(),
-        distance = countDown - now;
+//   const countDown = new Date(wc).getTime(),
+//     x = setInterval(function () {
+//       const now = new Date().getTime(),
+//         distance = countDown - now;
 
-      (document.getElementById("days").innerText = Math.floor(distance / day)),
-        (document.getElementById("hours").innerText = Math.floor(
-          (distance % day) / hour + 16
-        )),
-        (document.getElementById("minutes").innerText = Math.floor(
-          (distance % hour) / minute
-        ));
-    }, 0);
-})();
+//       (document.getElementById("days").innerText = Math.floor(distance / day)),
+//         (document.getElementById("hours").innerText = Math.floor(
+//           (distance % day) / hour
+//         )),
+//         (document.getElementById("minutes").innerText = Math.floor(
+//           (distance % hour) / minute
+//         ));
+//     }, 0);
+// })();
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 20, 2022 16:00:00").getTime();
+
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = document.getElementById("days").innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = document.getElementById("hours").innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var mins = document.getElementById("minutes").innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+}, 1000);
 
 
 // SWIPER
